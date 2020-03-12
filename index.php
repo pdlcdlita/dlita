e1e0bf20a3f9516e6f7746541b97a4425a6d8c98
pdlcdlita <61896543+pdlcdlita@users.noreply.github.com>
2020-03-12 20:22:46 +0530

    Dlita Invites

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "You are co-ordially invited for the Inauguration of D'LITA organised by Team D'LITA of the B.Sc.I.T Department";
+
+?>
---
+++ mcom.php
@@ -0,0 +1,48 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Invitation</title>
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/all.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/all.js"></script>
+	
+</head>
+<body>
+	<div class="jumbotron text-center p-10">
+		<img src="img/header.png">
+	</div>
+	<div class="content">
+		<div class="bg"></div>
+		<div class="course_name">
+			<h5><b>M.Com Department</b></h5>
+			<p>You are cordially invited for the <br>"Inauguration of D'LITA"<br> organised by Team D'LITA of the B.Sc.I.T Department</p>
+		</div>
+		<div class="details">
+			<div class="info">
+				<i class="fas fa-calendar-alt"></i>&nbsp;<b>DATE</b> 
+				<p>17th March, 2020</p>
+			</div>
+			<div class="info">
+				<i class="fas fa-clock"></i>&nbsp;<b>TIME</b> 
+				<p>10:00 AM</p>
+			</div>
+			<div class="info">
+				<i class="fas fa-map-marker-alt"></i>&nbsp;<b>VENUE</b> 
+				<p>College Auditorium</p>
+			</div>
+		</div>
+	</div>
+	<div class="rule"></div>
+	<footer>
+		<div style="width: 100%">
+			<iframe width="100%" height="200" src="https://maps.google.com/maps?width=100%&amp;height=200&amp;hl=en&amp;q=prahladrai%20dalmia%20lions%20college+(Inauguration%20of%20D'LITA)&amp;ie=UTF8&amp;t=&amp;z=17&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+		</div>
+	</footer>
+</body>
+</html>
